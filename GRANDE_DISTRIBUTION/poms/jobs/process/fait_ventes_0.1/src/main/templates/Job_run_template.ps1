d13130f621e1ce094d9c86a047f34ed7652ed9dd
ericdasse28 <christ.gououi@gmail.com>
2020-04-14 12:13:37 +0200

    Projet Talend finalisé
    Dans cette nouvelle version du projet, la jointure entre les tables article et nomenclature a été rajoutée, le job de création de la table de faits a été réalisée et les doublons dans les tables magasin et nomenclature ont été gérés
    
    Contrairement à ce qui a été annoncé, je le délivre sous la forme d'un dossier vu que le format .zip parait ne pas être admis par GitHub

---
+++ GRANDE_DISTRIBUTION/poms/jobs/process/fait_ventes_0.1/src/main/templates/Job_run_template.ps1
@@ -0,0 +1,3 @@
+$fileDir = Split-Path -Parent $MyInvocation.MyCommand.Path
+cd $fileDir
+java '-Dtalend.component.manager.m2.repository=%cd%/../lib' '-Xms256M' '-Xmx1024M' '-Dfile.encoding=UTF-8' -cp '.;../lib/routines.jar;../lib/commons-collections-3.2.2.jar;../lib/log4j-1.2.17.jar;../lib/mysql-connector-java-5.1.30-bin.jar;../lib/jboss-serialization.jar;../lib/jakarta-oro-2.0.8.jar;../lib/dom4j-1.6.1.jar;../lib/trove.jar;../lib/advancedPersistentLookupLib-1.2.jar;../lib/crypto-utils.jar;../lib/talend_file_enhanced_20070724.jar;fait_ventes_0_1.jar;' grande_distribution.fait_ventes_0_1.FAIT_Ventes  --context=Default %*
